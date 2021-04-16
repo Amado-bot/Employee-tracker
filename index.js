023614bc66fe60ef02e473d979b1d08ba0bc6f62
unknown <amadocardenas0@gmail.com>
2021-04-16 13:48:20 -0600

    added tables to the schema file according to what they would like to see in them.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world!!!");
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+async function init(){
+    const  answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'temp',
+        message: 'what is cow?'
+
+    }]);
+    console.log(answers);
+   const queryRsponse = await db.promise().query("SELECT * FROM voters;")
+   console.log(queryRsponse);
+}
+
+init();
