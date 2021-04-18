fd13eafe5433e1cdc7e3ccffaf5b25a7dd53e381
unknown <amadocardenas0@gmail.com>
2021-04-17 20:43:06 -0600

    less room for err

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const cTable = require('console.table');
 const db = mysql.createConnection({
     host: "localhost",
-    port: 3001,
+    port: 3306,
     user: "root",
     password: "password",
     database: "employeeDB"
