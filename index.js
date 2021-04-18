f8ba68086ce613e9547661e3c165753a38cbe214
unknown <amadocardenas0@gmail.com>
2021-04-17 19:43:59 -0600

    made db query for the view all functions

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function init() {
 }
 
 function viewAllEmployees() {
-    db.query("",
+    db.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
         function (err, res) {
             if (err) throw err
             console.log(res)
@@ -78,7 +78,7 @@ function viewAllEmployees() {
 }
 
 function viewAllRoles() {
-    db.query("",
+    db.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;",
         function (err, res) {
             if (err) throw err
             console.log(res)
@@ -87,7 +87,7 @@ function viewAllRoles() {
 }
 
 function viewAllDepartments() {
-    db.query("",
+    db.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",
         function (err, res) {
             if (err) throw err
             console.log(res)
@@ -120,24 +120,44 @@ function selectManagers() {
 
 function addEmployee() {
     inquirer.prompt([{
-        name: 'firstName',
-        type: 'input',
-        message: 'Enter First Name'
-    }, {
+            name: 'firstName',
+            type: 'input',
+            message: 'Enter First Name'
+        }, {
 
-        name: 'lastName',
-        type: 'input',
-        message: 'Enter Last Name'
-    }, {
-        name: 'role',
-        type: 'list',
-        message: 'Choose A Roll',
-        choices: selectRole()
-    }, {
-        name: 'choice',
-        type: 'rawlist',
-        message: 'Choose A Manager',
-        choices: selectmanager()
-    }])
+            name: 'lastName',
+            type: 'input',
+            message: 'Enter Last Name'
+        }, {
+            name: 'role',
+            type: 'list',
+            message: 'Choose A Roll',
+            choices: selectRole()
+        }, {
+            name: 'choice',
+            type: 'rawlist',
+            message: 'Choose A Manager',
+            choices: selectmanager()
+        }
+    ]).then(function (val) {
+        var roleId = selectRole().indexOf(val.role) + 1
+        var managerId = selectManager().indexOf(val.choice) + 1
+        connection.query("INSERT INTO employee SET ?", 
+        {
+            first_name: val.firstName,
+            last_name: val.lastName,
+            manager_id: managerId,
+            role_id: roleId
+            
+        }, function(err){
+            if (err) throw err
+            console.table(val)
+            init()
+        })
+    })
 }
+
+//updateEmployee//
+
+
 init();
